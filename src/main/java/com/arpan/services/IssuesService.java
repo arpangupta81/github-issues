8f7d1577bae68ae6ce24424889b27b4d7ec7075b
Arpan Gupta <arpangupta81@gmail.com>
2019-08-18 19:53:20 +0530

    Refactored the code.

--- src/main/java/com/arpan/services/IssuesService.java
+++ src/main/java/com/arpan/services/IssuesService.java
@@ -20,14 +20,17 @@ import java.util.Optional;
 
 import static com.arpan.util.IssuesUtil.createGitApiUrl;
 import static com.arpan.util.IssuesUtil.getNumberOfIssuesModel;
+import static java.util.Base64.getDecoder;
 
 @Slf4j
 @Service
 public class IssuesService {
+    private static final int ONE = 1;
+    private static final int HUNDRED = 100;
     private static final String ISSUES = "/issues";
     private final GitHubClient gitHubClient;
-    @Value("${auth-token:52fa4dd4ded5632849de453fce50d457921a7fce}")
-    private String githubToken;
+    @Value("${key:dummy-key}")
+    private String key;
 
     @Autowired
     public IssuesService() {
@@ -35,7 +38,7 @@ public class IssuesService {
     }
 
     private PageIterator<RepositoryIssue> pageIssues(String uri) {
-        PagedRequest<RepositoryIssue> request = new PagedRequest<>(1, 100);
+        PagedRequest<RepositoryIssue> request = new PagedRequest<>(ONE, HUNDRED);
         request.setUri(uri);
         request.setType(new TypeToken<List<RepositoryIssue>>() {
         }.getType());
@@ -43,7 +46,7 @@ public class IssuesService {
     }
 
     private <V> List<V> getAll(PageIterator<V> iterator, List<String> errorList) {
-        List<V> elements = new ArrayList<V>();
+        List<V> elements = new ArrayList<>();
         try {
             while (iterator.hasNext())
                 elements.addAll(iterator.next());
@@ -62,14 +65,15 @@ public class IssuesService {
      * @return Optional of Issues Model.
      */
     public Optional<NumberOfIssuesModel> getIssuesModel(String gitRepoUrl) {
-        gitHubClient.setOAuth2Token(githubToken);
-        List<String> errorList = new ArrayList<>();
+        gitHubClient.setOAuth2Token(new String(getDecoder().decode(key)));
 
+        List<String> errorList = new ArrayList<>();
         String gitApiUrl = createGitApiUrl(gitRepoUrl, ISSUES);
         if (Strings.isNullOrEmpty(gitApiUrl)) {
             log.error("Received Null or Empty URL");
             return Optional.empty();
         }
+
         List<RepositoryIssue> repositoryIssues = getAll(pageIssues(gitApiUrl), errorList);
         if (!errorList.isEmpty()) {
             return Optional.empty();
